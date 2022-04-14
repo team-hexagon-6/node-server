3748ce0520d2c0fc43487a78da5e03c835def72c
Ayesh Vininda <ayeshgk@gmail.com>
2022-04-15 01:53:42 +0530

    node-server:ayesh:add database for user and auth ,add seed ,add initials for auth ,site,user

---
+++ prisma/seed/users.ts
@@ -0,0 +1,32 @@
+export const users =[
+    {
+        user_id: '23334343A',
+        firstname: 'admin',
+        lastname: 'admin',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'admin@gmail.com',
+        birthday: new Date('2020-01-01'),
+        active: true,
+    },
+    {
+        user_id:"23334343E",
+        firstname: 'examiner',
+        lastname: 'examiner',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'examiner@gmail.com',
+        birthday: new Date('2020-01-01'),
+        active: true,
+    },
+    {
+        user_id:"23334343D",
+        firstname: 'doctor',
+        lastname: 'doctor',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'doctor@gmail.com',
+        birthday: new Date('2020-01-01'),
+        active: true,
+    }
+]
