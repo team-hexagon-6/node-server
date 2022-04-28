812f6ec87f9737b2af4b9a8ea7c3cd747523c417
Ayesh Vininda <ayeshgk@gmail.com>
2022-04-28 12:44:34 +0530

    ayesh:update password api

--- utils/validation.js
+++ utils/validation.js
@@ -33,8 +33,39 @@ const update_user_validation = (data) => {
     return schema.validate(data, { abortEarly: false });
 }
 
+const update_password_validation = (data) => {
+    const schema = Joi.object({
+        new_password: Joi.string().required().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')),
+        old_password: Joi.string().required().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')),
+        user_id: Joi.string().min(5).max(25).required()
+    })
+
+    return schema.validate(data, { abortEarly: false });
+}
+
+const password_validation = (data) => {
+    const schema = Joi.object({
+        password: Joi.string().required().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')),
+        user_id: Joi.string().min(5).max(25).required()
+    })
+
+    return schema.validate(data, { abortEarly: false });
+}
+
+const get_user_validation = (data) => {
+    const schema = Joi.object({
+        user_id: Joi.string().min(5).max(25).required()
+    })
+
+    return schema.validate(data, { abortEarly: false });
+}
+
+
 module.exports = {
     register_vaidation,
     login_validation,
-    update_user_validation
+    update_user_validation,
+    update_password_validation,
+    password_validation,
+    get_user_validation
 }
