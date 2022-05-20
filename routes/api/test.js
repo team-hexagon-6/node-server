5d430607cbdfcbdd595977b92e91aa8ecd12643d
Ayesh Vininda <ayeshgk@gmail.com>
2022-05-21 01:39:51 +0530

    test complete

--- routes/api/test.js
+++ routes/api/test.js
@@ -7,7 +7,7 @@ router
     .post('/create-new-test', verifyRoles(process.env.EXAMINER_ROLE), testController.handleNewTest)
     .post('/do-test', verifyRoles(process.env.EXAMINER_ROLE), testController.doTest)
     .get('/get-test/:test_id', verifyRoles(process.env.EXAMINER_ROLE, process.env.DOCTOR_ROLE), testController.getTest)
-    .get('/get-tests', verifyRoles(process.env.EXAMINER_ROLE, process.env.DOCTOR_ROLE), testController.getAllTests)
+    .get('/get-tests', verifyRoles(process.env.EXAMINER_ROLE, process.env.DOCTOR_ROLE), testController.getAllTestsForPatient)
     .get('/get-test-record/:test_record_id', verifyRoles(process.env.EXAMINER_ROLE, process.env.DOCTOR_ROLE), testController.getTestRecord)
     .get('/get-test-records', verifyRoles(process.env.EXAMINER_ROLE, process.env.DOCTOR_ROLE), testController.getAllTestRecords)
     .get('/test-types', verifyRoles(process.env.EXAMINER_ROLE), testController.getTestTypes);
