65995f6026a0b77b1f06761192e909a0aa87f8ba
Ayesh Vininda <ayeshgk@gmail.com>
2022-05-27 10:21:11 +0530

    seed/add users

--- prisma/seed/auths.ts
+++ prisma/seed/auths.ts
@@ -17,5 +17,159 @@ export const auths =[
         user_type_id:3,
         password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
         complete_profile: true,
-    }
+    },
+    // 
+    {
+        id:"23334sdf43D",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233343dfs3E",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333sdf4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233jghfg4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333fgh4343E",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"23334fjfh343D",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233tyrty34343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333fhfg4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233jnfgm34343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"5235442rerteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"42rt45sfd44543E",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333shgh4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333hjd4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233eht34343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333mazdcm4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233jejyj34343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"233msymy34343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"2333jrtjs4343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"23srm334343D",
+        user_type_id:3,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    //
+    {
+        id:"5235gdfdsfsfeE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"5235dsfsdfteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"523r7u4hrherteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"523474grgvteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"524w56fdgerteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+    {
+        id:"dgah4ydf42rerteE",
+        user_type_id:2,
+        password:"$2b$10$vn9AzLGJP9imTSOErEW.lOaSbvxReIJKwNJK6E4JIPlLUawJlDjmO",
+        complete_profile: true,
+    },
+
+    
 ]
