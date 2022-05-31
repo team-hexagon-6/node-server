c24e6d25d4eb189a851777f0c35fc6764f4a032c
Ayesh Vininda <ayeshgk@gmail.com>
2022-05-31 21:22:31 +0530

    patients added

--- prisma/seed/patients.ts
+++ prisma/seed/patients.ts
@@ -39,4 +39,155 @@ export const patients =[
         birthday: new Date('2020-01-01'),
         gender_type_id: 2
     },
+    // 
+    {
+        id: 'P23334374',
+        firstname: 'patient_5',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334375',
+        firstname: 'patient_6',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334376',
+        firstname: 'patient_7',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334377',
+        firstname: 'patient_8',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334378',
+        firstname: 'patient_9',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334379',
+        firstname: 'patient_10',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334380',
+        firstname: 'patient_11',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334381',
+        firstname: 'patient_12',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334382',
+        firstname: 'patient_13',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334383',
+        firstname: 'patient_14',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334384',
+        firstname: 'patient_15',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334385',
+        firstname: 'patient_16',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334386',
+        firstname: 'patient_17',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334387',
+        firstname: 'patient_18',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
+    {
+        id: 'P23334388',
+        firstname: 'patient_19',
+        lastname: 'patient_1_1',
+        nic: '91234567v',
+        contact_no: '0771234567',
+        email:  'p1@gmail.com',
+        birthday: new Date('2020-01-01'),
+        gender_type_id: 1
+    },
 ]
